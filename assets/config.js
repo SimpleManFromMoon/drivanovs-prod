bd856497c4e9bfe8deb7fa721733afb0275e98f5
Maksims Ivanovs <ivanovapasts@icloud.com>
2025-09-29 14:47:02 +0300

    time slots fix

--- assets/config.js
+++ assets/config.js
@@ -6,11 +6,11 @@
   const CONFIG_MAP = {
     local: {
       ORIGIN: 'http://localhost:8080',
-      ENDPOINT: 'https://script.google.com/macros/s/AKfycbysAVU_42zvb9xkdN-frHOO3tJTYREW9kchz2iI0AmwTqZ9k8HR0yWDsIwF3IeDL_V1/exec'
+      ENDPOINT: 'https://script.google.com/macros/s/AKfycbytXlQA7WxAQXBrydjRfGqZrg1mt8WII7FsGJYwCYlt3QMAs0qEudR8VgAThycGPhfplQ/exec'
     },
     prod: {
       ORIGIN: 'https://simplemanfrommoon.github.io/drivanovs-prod/',
-      ENDPOINT: 'https://script.google.com/macros/s/AKfycbysAVU_42zvb9xkdN-frHOO3tJTYREW9kchz2iI0AmwTqZ9k8HR0yWDsIwF3IeDL_V1/exec'
+      ENDPOINT: 'https://script.google.com/macros/s/AKfycbytXlQA7WxAQXBrydjRfGqZrg1mt8WII7FsGJYwCYlt3QMAs0qEudR8VgAThycGPhfplQ/exec'
     }
   };
 
